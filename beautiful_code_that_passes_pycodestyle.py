d5864a35773f2710fae01dab00d7d91e5f3998b5
Blossom Ayogu <ayogublossomisrael@gmail.com>
2023-07-17 00:00:37 +0000

    Fix pycodestyle error

--- beautiful_code_that_passes_pycodestyle.py
+++ beautiful_code_that_passes_pycodestyle.py
@@ -1,11 +1,13 @@
 """a module that contains code that passes the pycodestyle check"""
 
+
 class DemoClass:
     """This is a demo class that shows how to document a class"""
-    
+
     def demo_method(self):
         """This is a demo method that shows how to document a method"""
         pass
+
 
 def demo_function():
     """A demo function that shows how to document a function"""
